122509093cdc02a618bb43a35097726881d5ff82
Kiran Panesar <kiran@mobilexlabs.com>
2014-05-21 22:10:22 +0100

    Allow manual show/hide of status bar

---
+++ Sources/UINavigationController+NavBarAnimation.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+NavBarAnimation.h
+//  Reader
+//
+//  Created by Kiran Panesar on 21/05/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (NavBarAnimation)
+
+@end
---
+++ Sources/UINavigationController+NavBarAnimation.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+NavBarAnimation.m
+//  Reader
+//
+//  Created by Kiran Panesar on 21/05/2014.
+//
+//
+
+#import "UINavigationController+NavBarAnimation.h"
+
+@implementation UINavigationController (NavBarAnimation)
+
+@end
